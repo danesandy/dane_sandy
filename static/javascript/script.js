7fac5e7c3d747e6b5532343eeac2768909e89368
Dane Sandy <danesandy10@gmail.com>
2022-04-12 23:54:00 -0400

    cleaned up code

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -6,3 +6,24 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
     // navbar.classList.toggle('nav-style');
 });
+
+// Contact Form Init
+(function() {
+    emailjs.init('user_hBlvDdJ4qtYa66auYld3H');
+})();
+
+// Contact Form
+window.onload = function() {
+    var x = document.getElementById("contact_alert");
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        emailjs.sendForm('service_wu9efji', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                $('#contact_form_modal').modal('hide');                    
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+        });
+    };
